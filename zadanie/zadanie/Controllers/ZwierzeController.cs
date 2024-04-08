d1d6874549e517d62af276df0c1b1e86841b58ba
s28087 <28087@pjwstk.edu.pl>
2024-04-08 17:12:53 +0200

    Zwierzeta wraz z funkcjami

---
+++ zadanie/zadanie/Controllers/ZwierzeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using zadanie.Models;
+
+namespace zadanie.Controllers;
+
+[Route("api/zwierzeta")]
+[ApiController]
+public class ZwierzeController : ControllerBase
+{
+    
+    private static readonly List<Zwierze> _zwierzeta = new()
+    {
+        new Zwierze { IdZwierze = 1, Imie = "Azor", Kategoria = Kategoria.pies, Masa = 15.0, KolorSiersci = "biały" },
+        new Zwierze { IdZwierze = 2, Imie = "Tosia", Kategoria = Kategoria.kot, Masa = 10.0, KolorSiersci = "czarny" }
+    };
+
+    [HttpGet]
+    public IActionResult GetZwierze()
+    {
+        return Ok(_zwierzeta);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetZwierze(int id)
+    {
+        var zwierze = _zwierzeta.FirstOrDefault(zwierze1 => zwierze1.IdZwierze == id);
+
+        if (zwierze == null)
+        {
+            return NotFound("Zwierze z id " + id + " nie zostało znalezione");
+        }
+
+        return Ok(zwierze);
+
+    }
+
+    [HttpPost]
+    public IActionResult CreateZwierze(Zwierze zwierze)
+    {
+        _zwierzeta.Add(zwierze);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateZwierze(int id, Zwierze zwierze)
+    {
+        var zwierzeEdycja = _zwierzeta.FirstOrDefault(zwierze1 => zwierze1.IdZwierze == id);
+
+        if (zwierzeEdycja == null)
+        {
+            return NotFound("Zwierze z id " + id + " nie zostało znalezione");
+        }
+
+
+        _zwierzeta.Remove(zwierzeEdycja);
+        _zwierzeta.Add(zwierze);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteZwierze(int id)
+    {
+        var zwierzeUsuniecie = _zwierzeta.FirstOrDefault(zwierze1 => zwierze1.IdZwierze == id);
+
+        if (zwierzeUsuniecie == null)
+        {
+            return NotFound("Zwierze z id " + id + " nie zostało znalezione");
+        }
+
+
+        _zwierzeta.Remove(zwierzeUsuniecie);
+        return NoContent();
+    }
+    
+
+}
---
+++ zadanie/zadanie/Models/Zwierze.cs
@@ -0,0 +1,22 @@
+﻿namespace zadanie.Models;
+
+public enum Kategoria
+{
+    pies,
+    kot,
+    gryzon,
+    gad,
+    plaz
+}
+
+public class Zwierze
+{
+    public int IdZwierze { get; set; }
+    public string Imie { get; set; }
+    public Kategoria Kategoria{ get; set; }
+    public double Masa { get; set; }
+    public string KolorSiersci { get; set; }
+    
+    
+    
+}
